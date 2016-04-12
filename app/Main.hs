0de01a4a7170c088f1d14e203c356fa0bda7f2cc
scturtle <scturtle@gmail.com>
2016-04-12 13:10:16 +0800

    switch to spock

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Data.Text.Lazy (toStrict)
 import Control.Monad.Reader
 import System.Environment (lookupEnv)
-import Web.Scotty
+import Web.Spock.Safe
 import Network.Wai.Middleware.Static
 import Network.Wai.Middleware.RequestLogger
-import Hathverse.Db (runSql, runQuery)
+import qualified Hathverse.Db as Db
 import Hathverse.Controller
 
 main :: IO ()
-main = runSql $ \pool -> do
+main = Db.runSql $ \pool -> do
 
   port <- maybe 3000 read <$> lookupEnv "PORT"
 
-  scotty port $ do
+  runSpock port $ spockT id $ do
 
     middleware logStdoutDev
     middleware $ staticPolicy $ addBase "static"
 
-    get "/" $ liftIO (runQuery pool homepage) >>= html
+    get root $
+       html . toStrict =<< liftIO (Db.runQuery pool homepage)
 
-    get "/problems/:pid" $ do
-      pid <- read <$> param "pid"
-      liftIO (runQuery pool $ problemPage pid) >>= html
+    get ("problems" <//> var) $ \pid ->
+       html . toStrict =<< liftIO (Db.runQuery pool $ problemPage pid)
 
     post "/check" $ do
-      j <- jsonData
-      liftIO (runQuery pool $ checkApi j) >>= json
+      j <- jsonBody'
+      json =<< liftIO (Db.runQuery pool $ checkApi j)
