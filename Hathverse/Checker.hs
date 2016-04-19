3e7d4d88e1e1b23547d4a5f62533ed8cdbe7f8e3
Javran Cheng <Javran.c@gmail.com>
2016-04-19 01:25:16 -0400

    do proper cleanup on exceptions

--- Hathverse/Checker.hs
+++ Hathverse/Checker.hs
@@ -1,6 +1,9 @@
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Hathverse.Checker where
 
 import GHC.Generics (Generic)
@@ -15,6 +18,8 @@ import Data.Unique
 import Data.Aeson
 import System.Directory
 import Hathverse.Db
+import Control.Exception
+import Data.Functor
 
 data CheckResult = CheckResult {
     ok :: Bool
@@ -23,58 +28,76 @@ data CheckResult = CheckResult {
 
 instance ToJSON CheckResult
 
--- | Well, stolen from 99haskell...
+ -- | Well, stolen from 99haskell...
 check :: Problem -> String -> IO CheckResult
-check Problem{..} code = do
+check Problem{..} code =
+    bracket prepareCheck cleanupResource performCheck
+  where
+    prepareCheck :: IO (String, FilePath, Either IOException _)
+    prepareCheck = do
+      submissionId <- ('h':) . show . hashUnique <$> newUnique
+      homeDir <- getHomeDirectory
+      let tmp = if os == "linux" then "/tmp" else homeDir
+          dir = tmp ++ "/hathverse/" ++ submissionId
 
-  submissionId <- ('h':) . show . hashUnique <$> newUnique
-  homeDir <- getHomeDirectory
-  let tmp = if os == "linux" then "/tmp" else homeDir
-      dir = tmp ++ "/hathverse/" ++ submissionId
+      createDirectoryIfMissing True dir
+      writeFile (dir ++ "/" ++ T.unpack problemModuleName ++ ".hs") code
+      writeFile (dir ++ "/check.hs") $ T.unpack problemCheckProgram
 
-  createDirectoryIfMissing True dir
-  writeFile (dir ++ "/" ++ T.unpack problemModuleName ++ ".hs") code
-  writeFile (dir ++ "/check.hs") $ T.unpack problemCheckProgram
+      let dockerArgs =
+            [ "run"
+            , "--name=" ++ submissionId
+            , "--interactive=true"
+            , "--net=none" -- no network
+            -- TODO: limit CPU/memory
+            , "--volume=" ++ dir ++ ":/mnt"
+            , "scturtle/hathverse:alpine" -- image name
+            ]
+      eResult <- try $
+         createProcess
+           (proc "docker" dockerArgs)
+             { std_in = CreatePipe
+             , std_out = CreatePipe
+             , std_err = CreatePipe }
+      pure (submissionId, dir, eResult)
 
-  (Just hin, Just hout, Just herr, hproc) <-
-    createProcess
-      (proc "docker" [ "run"
-                     , "--name=" ++ submissionId
-                     , "--interactive=true"
-                     , "--net=none" -- no network
-                     -- TODO: limit CPU/memory
-                     , "--volume=" ++ dir ++ ":/mnt"
-                     , "scturtle/hathverse:alpine" -- image name
-                     ])
-      { std_in = CreatePipe , std_out = CreatePipe , std_err = CreatePipe }
+    cleanupResource :: _ -> IO ()
+    cleanupResource (submissionId, dir, eResult) = do
+      case eResult of
+        Right (Just hIn, Just hOut, Just hErr, hProc) -> do
+          -- closing a handler more than once is not considered an error,
+          -- while this function does not know which handler has been closed,
+          -- let's just close them all.
+          mapM_ hClose [hIn, hOut, hErr]
+          terminateProcess hProc
+        _ -> pure ()
+      void $ async $ do
+        result <- try $ readProcess "docker" ["rm", "-f", submissionId] ""
+        case result of
+          Left (e :: IOException) -> putStrLn $ "error while cleaning up: " ++ show e
+          _ -> pure ()
+        removeDirectoryRecursive dir
 
-  mapM_ (hPutStrLn hin)
-    [ "cd /tmp"
-    , "cp /mnt/* ."
-    , "ghc -Wall -O2 check.hs 2>&1"
-    , "./check 2>&1"
-    ]
-  hClose hin
-
-  maybeOutput <- timeout (60 * 1000000) (Strict.hGetContents hout)
-
-  let (output, ok) =
-        case maybeOutput of
-          Nothing ->
-            ("Timeout.", False)
-          Just output ->
-            let ok = (not . null) output &&
-                  case words . last . lines $ output of
-                    [_, _, "0", "failures"] -> True
-                    _ -> False
-            in (output, ok)
-
-  hClose hout
-  hClose herr
-  terminateProcess hproc
-
-  _ <- async $ do
-    _ <- readProcess "docker" ["rm", "-f", submissionId] ""
-    removeDirectoryRecursive dir
-
-  return CheckResult { ok=ok, output=output }
+    performCheck :: _ -> IO CheckResult
+    performCheck (_, _, Left e) = do
+      let errorMsg = "server side failure: " ++ show (e :: IOException)
+      putStrLn errorMsg
+      pure $ CheckResult False errorMsg
+    performCheck (_, _, Right handlers) = do
+      let (Just hin, Just hout, _, _) = handlers
+      mapM_ (hPutStrLn hin)
+        [ "cd /tmp"
+        , "cp /mnt/* ."
+        , "ghc -Wall -O2 check.hs 2>&1"
+        , "./check 2>&1"
+        ]
+      hClose hin
+      maybeOutput <- timeout (60 * 1000000) (Strict.hGetContents hout)
+      case maybeOutput of
+        Nothing -> pure $ CheckResult False "Timeout."
+        Just output ->
+               let ok = (not . null) output &&
+                     case words . last . lines $ output of
+                       [_, _, "0", "failures"] -> True
+                       _ -> False
+               in pure $ CheckResult ok output
