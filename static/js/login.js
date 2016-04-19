c5bd176bea9062697387adce1505659267dc8c40
Javran Cheng <Javran.c@gmail.com>
2016-04-19 02:13:25 -0400

    hide message when opening problem & refactor

--- static/js/login.js
+++ static/js/login.js
@@ -1,20 +1,24 @@
-function callback(type){
-  return function(){
-    $.ajax({url: "/login",
-            type: "POST",
-            data: {"username": $("#username").val(),
-                   "password": $("#password").val(),
-                   "type": type},
-            success: function(data){
-              if(data.ok){
-                window.location.href = "/";
-              } else {
-                $("#err-msg").html(data.err);
-                $("#err-msg").show();
-              }
-            }});
-  }
-}
+$(document).ready( function() {
+    "use strict";
 
-$("#login-btn").click(callback("login"));
-$("#signup-btn").click(callback("signup"));
+    function callback(type){
+        return function(){
+            $.ajax({url: "/login",
+                    type: "POST",
+                    data: {"username": $("#username").val(),
+                           "password": $("#password").val(),
+                           "type": type},
+                    success: function(data){
+                        if(data.ok){
+                            window.location.href = "/";
+                        } else {
+                            $("#err-msg").html(data.err);
+                            $("#err-msg").show();
+                        }
+                    }});
+        }
+    }
+
+    $("#login-btn").click(callback("login"));
+    $("#signup-btn").click(callback("signup"));
+});
--- static/js/problem.js
+++ static/js/problem.js
@@ -1,33 +1,39 @@
-window.onload = function() {
-  var codeblock = $("#code");
-  var template = codeblock.html();
-  codeblock.html("");
-  editor = CodeMirror($("#code")[0], {
-    mode: "haskell",
-    value: template,
-    lineNumbers: true
-  });
-}
+$(document).ready( function() {
+    "use strict";
+    var codeblock = $("#code");
+    var template = codeblock.html();
+    codeblock.html("");
+    var editor = CodeMirror($("#code")[0], {
+        mode: "haskell",
+        value: template,
+        lineNumbers: true
+    });
 
-$("#run").click(function(){
-  if($("#run").hasClass("disabled")) return;
-  $("#run").addClass("disabled");
-  var pid = document.URL.substr(document.URL.lastIndexOf('/')+1);
-  pid = parseInt(pid);
-  var code = editor.getValue();
-  $.ajax({url: "/check",
-          type: "POST",
-          data: JSON.stringify({"probId": pid, "solCode": code}),
-          contentType: "application/json",
-          success: function(data){
-            $("#results").removeClass("alert-info alert-danger alert-success");
-            if(data.ok){
-              $("#results").addClass("alert-success");
-              $("#result").html("Well done!");
-            } else {
-              $("#results").addClass("alert-danger");
-              $("#result").html(data.output);
-            }
-            $("#run").removeClass("disabled");
-          }});
+    $("#results").hide();
+
+    $("#run").click(function(){
+        if($("#run").hasClass("disabled")) return;
+        $("#run").addClass("disabled");
+        var pid = document.URL.substr(document.URL.lastIndexOf('/')+1);
+        pid = parseInt(pid);
+        var code = editor.getValue();
+        $.ajax({url: "/check",
+                type: "POST",
+                data: JSON.stringify({"probId": pid, "solCode": code}),
+                contentType: "application/json",
+                success: function(data){
+                    $("#results")
+                        .removeClass("alert-info alert-danger alert-success")
+                        .show();
+
+                    $("#results").addClass(data.ok 
+                                           ? "alert-success"
+                                           : "alert-danger");
+                    
+                    $("#result").text(data.ok
+                                      ? "Well done!"
+                                      : (data.output || "<empty content>"));
+                    $("#run").removeClass("disabled");
+                }});
+    });
 });
