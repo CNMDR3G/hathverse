863559e8a5575186fc167ed2506ddd570cd2daf6
Javran Cheng <Javran.c@gmail.com>
2016-04-22 15:39:12 -0400

    closes #9

--- static/js/problem.js
+++ static/js/problem.js
@@ -9,12 +9,30 @@ $(document).ready( function() {
         lineNumbers: true
     });
 
+    console.assert(typeof localStorage !== "undefined",
+                   "warning: localStorage not available.");
+
+    var codes = localStorage.codes 
+        ? JSON.parse(localStorage.codes) : {};
+
+    var pid = document.URL.substr(document.URL.lastIndexOf('/')+1);
+    pid = parseInt(pid);
+
+    // restore saved code for this problem if any
+    var thisCode = codes["p"+pid];
+    if (typeof thisCode !== "undefined")
+        editor.setValue(thisCode);
+
     $("#run").click(function(){
         if($("#run").hasClass("disabled")) return;
         $("#run").addClass("disabled");
-        var pid = document.URL.substr(document.URL.lastIndexOf('/')+1);
-        pid = parseInt(pid);
+
         var code = editor.getValue();
+
+        // save source code on submission
+        codes["p" + pid] = code;
+        localStorage.codes = JSON.stringify(codes);
+
         $.ajax({url: "/check",
                 type: "POST",
                 data: JSON.stringify({"probId": pid, "solCode": code}),
